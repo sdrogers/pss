dd8cd268ccdcc51401621474715e93575f8a015b
Tom Jackson <nightwinger101@hotmail.com>
2021-05-29 15:33:52 +0100

    updated code to check winner

--- pss_code/pss_utils.py
+++ pss_code/pss_utils.py
@@ -3,17 +3,29 @@
 PAPER = 0
 SCISSORS = 1
 STONE = 2
+VALID_MOVES = [PAPER, SCISSORS, STONE]
 
 #method to check winner of the round
 
-def check_winner(p1Choice, p2Choice):
-    if p1Choice == p2Choice + 1:
+def check_winner(p1_choice, p2_choice):
+    assert p1_choice in VALID_MOVES
+    assert p2_choice in VALID_MOVES
+    if p1_choice == p2_choice + 1:
         winner = 1
-    elif p1Choice == p2Choice - 2:
+    elif p1_choice == p2_choice - 2:
         winner = 1
-    elif p2Choice == p1Choice + 1:
+    elif p2_choice == p1_choice + 1:
         winner = -1
-    elif p2Choice == p1Choice - 2:
+    elif p2_choice == p1_choice - 2:
         winner = -1
-    elif psChoice == p1Choice:
+    elif p2_choice == p1_choice:
         winner = 0
+    return winner
+    
+if __name__ == "__main__":
+    check_winner(PAPER, SCISSORS)
+    assert winner == -1
+    check_winner(STONE, PAPER)
+    assert winner == 1
+    check_winner(SCISSORS, SCISSORS)
+    assert winner == 0

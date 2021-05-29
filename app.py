901bf6a02553f0c9e041de26d24866b3a4e21b09
Simon Rogers <simon.d.rogers@gmail.com>
2021-05-29 15:24:11 +0100

    Started first flask app
    - made templates folder
    - added index and main templates
    - added form to index page asking for name
    - on submission name is added to cookies and a new placeholder page is returned

--- app.py
+++ app.py
@@ -1,12 +1,27 @@
+import json
+
 from flask import Flask
+from flask import render_template, make_response
+from flask import request
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return "hello world"
+def index():
+    return render_template('index.html')
     
-    
+@app.route('/addname', methods=['POST', 'GET'])
+def addname():
+    if request.method == 'POST':
+        name = request.form['name']
+        resp = make_response(render_template('main.html', name=name))
+        resp.set_cookie('name', json.dumps(name))
+    else:
+        resp = render_template('index.html')
+    return resp
+
+
+
 @app.route('/hello/<name>')
 def goodbye_world(name):
     return "goodbye " + name

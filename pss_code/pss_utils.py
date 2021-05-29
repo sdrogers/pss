0a4f204e011d889d929b9ab78c9365f1d5dafaa7
Tom Jackson <nightwinger101@hotmail.com>
2021-05-29 15:07:23 +0100

    added method to check winner

--- pss_code/pss_utils.py
+++ pss_code/pss_utils.py
@@ -3,3 +3,17 @@
 PAPER = 0
 SCISSORS = 1
 STONE = 2
+
+#method to check winner of the round
+
+def check_winner(p1Choice, p2Choice):
+    if p1Choice == p2Choice + 1:
+        winner = 1
+    elif p1Choice == p2Choice - 2:
+        winner = 1
+    elif p2Choice == p1Choice + 1:
+        winner = -1
+    elif p2Choice == p1Choice - 2:
+        winner = -1
+    elif psChoice == p1Choice:
+        winner = 0

81649da4760ade44693ef5c2ca26aa3dba8ec3ce
Simon Rogers <simon.d.rogers@gmail.com>
2021-05-29 16:02:16 +0100

    added comments to app.py

--- app.py
+++ app.py
@@ -6,10 +6,14 @@ from flask import request
 
 app = Flask(__name__)
 
+# Main landing page - asks for name of user
 @app.route('/')
 def index():
     return render_template('index.html')
-    
+
+# Called from index with players name
+# adds name to cookie
+# returns main.html, passing name
 @app.route('/addname', methods=['POST', 'GET'])
 def addname():
     if request.method == 'POST':
